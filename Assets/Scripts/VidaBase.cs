a2a56153d5c896306ff7a28c67033abd6c1f7adc
1120150078 <1120150078@utch.edu.mx>
2025-02-11 19:26:13 -0600

    Se crea prefab de Alumno a ver si funciona

---
+++ Assets/Scripts/VidaBase.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class VidaBase : MonoBehaviour
+{
+    [SerializeField] private float saludInicial;
+    [SerializeField] private float saludMax;
+    public float Salud { get; protected set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+
+    }
+
+    public void RecibirDaño(float cantidad)
+    {
+        if (cantidad <= 0)
+        {
+            return;
+        }
+        if (Salud > 0f)
+        {
+            Salud -= cantidad;
+            ActualizarBarraVida(Salud, saludMax);
+
+            if (Salud <= 0f)
+            {
+                ActualizarBarraVida(Salud, saludMax);
+                PersonajeDerrotado();
+            }
+        }
+    }
+
+    protected virtual void ActualizarBarraVida(float vidaActual, float vidaMax)
+    {
+
+    }
+    protected virtual void PersonajeDerrotado()
+    {
+
+    }
+
+}
