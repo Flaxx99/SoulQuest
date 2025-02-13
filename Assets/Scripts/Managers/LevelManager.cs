6d660201454cc75067bb76547c6c93b37fd0beed
Yoshiry Q <a1120150078@utch.edu.mx>
2025-02-12 20:26:47 -0600

    Personaje muere

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private Personaje personaje;
+    [SerializeField] private Transform puntoReaparicion;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (personaje.PersonajeVida.Derrotado)
+            {
+                personaje.transform.localPosition = puntoReaparicion.position;
+                personaje.RestaurarPersonaje();
+            }
+        }
+        
+    }
+}
