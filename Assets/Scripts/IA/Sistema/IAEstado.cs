79111cda28911e0794ffd37a3f636d174b36a5d8
Jonathan <1120150052@utch.edu.mx>
2025-02-24 20:42:19 -0600

    Avance de un enemigo 😀

---
+++ Assets/Scripts/IA/Sistema/IAEstado.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "IA/Estado")]
+public class IAEstado : ScriptableObject
+{
+    public IAAccion[] Acciones;
+    public IATransicion[] Transiciones;
+
+    public void EjecutarEstado(IAController controller)
+    {
+        EjecutarAcciones(controller);
+        RealizarTransiciones(controller);
+    }
+    private void EjecutarAcciones(IAController controller)
+    {
+        if (Acciones == null || Acciones.Length <= 0)
+        {
+            return;
+        }
+        
+        for (int i = 0; i < Acciones.Length; i++)
+        {
+            Acciones[i].Ejecutar(controller);
+        }
+    }
+
+    private void RealizarTransiciones(IAController controller)
+    {
+        if (Transiciones == null || Transiciones.Length <= 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < Transiciones.Length; i++)
+        {
+            bool DecisionValor = Transiciones[i].Desicion.Decidir(controller);
+            if (DecisionValor)
+            {
+                controller.CambiarEstado(Transiciones[i].EstadoVerdadero);
+            }
+            else 
+            {
+                controller.CambiarEstado(Transiciones[i].EstadoFalso);
+            }
+        }
+    }
+}
